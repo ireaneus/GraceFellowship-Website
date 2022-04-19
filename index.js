45c39c71112efbf94860e0b18b6c600e020883e1
ireaneus <bwdave@protonmail.com>
2022-04-19 08:12:53 -0600

    Finally working to add a single sermon entry

--- index.js
+++ index.js
@@ -3,7 +3,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 const _ = require("lodash");
+const jsmediatags = require('jsmediatags');
 
 const app = express();
 
@@ -15,20 +17,51 @@ app.use(express.static("public"));
 // Mongoose connect, schema, and model section
 mongoose.connect("mongodb+srv://bwDave:TheRockCriesOut5@sermons0.ikt0p.mongodb.net/sermonsDB");
 
-const jsmediatags = require("jsmediatags");
-
-jsmediatags.read("sermons/1cor/1corinthians-part3.mp3", {
-  onSuccess: function(tag) {
-    var tags = tag.tags;
-    console.log(tags.title + " - " + tags.artist + " - " + tags.album + " - " + tags.year + " - " + tags.comment.text);
-  }
+const sermonsSchema = new mongoose.Schema({
+  _id: Number,
+  title: String,
+  artist: String,
+  album: String,
+  year: Number,
+  comment: String
 });
 
-const sermonsSchema = new mongoose.Schema({
-    title: tags.title,
-    artist: tags.ara
+const Sermon = mongoose.model("Sermon", sermonsSchema);
 
-})
+/*
+const booksSchema = new mongoose.Schema({
+  topic: String,
+  book: String,
+  path: "sermons/" + String
+});
+
+const Book = mongoose.model("Book", booksSchema);
+*/
+
+jsmediatags.read("sermons/1cor/1corinthians-part3.mp3", {
+    onSuccess: function(tag) {
+      var tags = tag.tags;
+      console.log(tags.title + " - " + tags.artist + " - " + tags.album + " - " + tags.year + " - " + tags.comment.text);
+
+      const file = new Sermon({
+        _id: 1,
+        title: tags.title,
+        artist: tags.artist,
+        album: tags.album,
+        year: tags.year,
+        comment: tags.comment.text
+      });
+
+      file.save();
+
+    },
+    onError: function(error) {
+      console.log(':(', error.type, error.info);
+    }
+  });
+
+
+
 
 
 let port = process.env.PORT;
